d9f99fdb68f36dcab52f68d7dfbb12e2556f8f84
Dotman-Robotics <aaron_jt05@hotmail.com>
2026-03-19 02:38:23 +1100

    Vectorized_Database_Agent
    Project utilizing two agents. One managing prompts and one managing csvs

---
+++ Vectorized_Database_Agent/main.py
@@ -0,0 +1,27 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriver
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an expert in answering questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question to answer: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n\n---------------------------------")
+    question = input("Ask your Question (q to quit): ")
+    print("\n\n---------------------------------")
+    if question == "q":
+        break
+
+    reviews = retriver.invoke(question) #Sim Search Algo
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
